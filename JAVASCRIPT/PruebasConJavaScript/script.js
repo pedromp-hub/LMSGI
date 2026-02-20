9a6b5b08a573a051dca2a7873f82013090bbd66a
pedromp-hub <pmonpel2304@g.educaand.es>
2026-02-20 11:12:52 +0100

    Practicando con JAVAScript

---
+++ JAVASCRIPT/PruebasConJavaScript/script.js
@@ -0,0 +1,7 @@
+
+        function oscuro(){
+            const texto = document.getElementById("texto");
+            texto.style.color = "White";
+            document.body.style.backgroundColor = "black";
+            texto.style.fontWeight = "bold";
+        }

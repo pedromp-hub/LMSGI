b85ffb4e5020374ec37d02e1b33c4ac4ac33fb59
pedromp-hub <pmonpel2304@g.educaand.es>
2026-02-25 14:35:21 +0100

    maspruebas JAVASCRIPT

--- JAVASCRIPT/PruebasConJavaScript/prueba4/script.js
+++ JAVASCRIPT/PruebasConJavaScript/prueba4/script.js
@@ -1,14 +1,47 @@
 const button = document.getElementById('agregar');
-const tareas = document.getElementById('tareas');
+const listatareas = document.getElementById('listatareas');
+const bcambiarcolor = document.getElementById('cambiarcolor');
+
+/*const bborrar = document.getElementById('borrar');*/
+
+
 const entrada = document.getElementById('entrada');
 
 button.addEventListener('click', agregar);
+bcambiarcolor.addEventListener('click', colorrojo);
+/*bborrar.addEventListener('click', borrar)*/
+
+function colorrojo(){
+    const listatareasli = document.querySelectorAll("li");
+    for (let i=0; i<listatareasli.length; i++){
+        listatareasli[i].style.color="red";
+    }
+}
 
 function agregar(){
-    const ptarea = document.createElement("p");
+    const litarea = document.createElement("li");
+
     const node = document.createTextNode(entrada.value);
 
-    ptarea.appendChild(node);
+    litarea.appendChild(node);
 
-    tareas.appendChild(ptarea);
+    listatareas.appendChild(litarea);
+
+
+    /*const enlace = document.createElement("a");
+    const node2 = document.createTextNode(" Enlace a....");
+    enlace.appendChild(node2);
+    enlace.setAttribute("href", "https://www.ieszaidinvergeles.org")
+
+    ptarea.appendChild(enlace);
+
+    const imagen = document.createElement("img");
+    tareas.appendChild(imagen);
+    imagen.setAttribute("src", "https://static.vecteezy.com/system/resources/previews/006/596/424/non_2x/waving-flag-of-catalan-independentist-estelada-vector.jpg");*/
+
 }
+
+/*function borrar(){
+    ptarea.remove();
+
+}*/

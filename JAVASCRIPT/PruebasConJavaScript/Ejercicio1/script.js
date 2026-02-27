6f9753997486fda6cfebd96bc382add3b33ae8bb
pedromp-hub <pmonpel2304@g.educaand.es>
2026-02-27 11:12:19 +0100

    Complemento de intentos ejercicio1

--- JAVASCRIPT/PruebasConJavaScript/Ejercicio1/script.js
+++ JAVASCRIPT/PruebasConJavaScript/Ejercicio1/script.js
@@ -1,6 +1,7 @@
 const button = document.getElementById('boton1');
 const input = document.getElementById('Intento');
-const p = document.getElementById('respuesta'); 
+const p = document.getElementById('respuesta');
+let intentos = 0; 
 
 const numero = Math.floor(Math.random() * 10)+ 1;
 
@@ -9,11 +10,14 @@ function adivinar() {
     const valor = Number(input.value);
 
     if (valor == numero) {
-        p.textContent = '¡¡Número Correcto!!';
+        intentos++;
+        respuesta.textContent = '¡¡Número Correcto!!' + ' Lo has conseguido en ' + intentos + ' Intentos' ;
     } else if (valor > numero) {
-        p.textContent = 'El número que tienes que adivinar es más pequeño que el que has introducido';
+        intentos++;
+        respuesta.textContent = 'El número que tienes que adivinar es más pequeño que el que has introducido. ' + ' Llevas ' + intentos + ' intentos';
     } else if (valor < numero) {
-        p.textContent = 'El número que tienes que adivinar es más grande que el que has introducido.';
+        intentos++;
+        respuesta.textContent = 'El número que tienes que adivinar es más grande que el que has introducido. ' + ' Llevas ' + intentos + ' intentos';
     }
 }
 

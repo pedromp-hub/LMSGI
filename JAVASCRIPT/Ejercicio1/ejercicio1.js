d24936d6589ca7bd61a5bf4a246a8062d96a4109
pedromp-hub <pmonpel2304@g.educaand.es>
2026-03-04 14:07:56 +0100

    Ejercicio1-Con una sola funcion

--- JAVASCRIPT/Ejercicio1/ejercicio1.js
+++ JAVASCRIPT/Ejercicio1/ejercicio1.js
@@ -4,27 +4,13 @@ const boton1 = document.getElementById("boton1");
 const boton2 = document.getElementById("boton2");
 const boton3 = document.getElementById("boton3");
 
-boton1.addEventListener('click', fboton1);
-boton2.addEventListener('click', fboton2);
-boton3.addEventListener('click', fboton3);
+boton1.addEventListener('click', function(){fboton(0)});
+boton2.addEventListener('click', function(){fboton(1)});
+boton3.addEventListener('click', function(){fboton(2)});
 
-function fboton1(){
+function fboton(index){
     for(let i=0; i<carrusel.length; i++){
         carrusel[i].style.display = "none";
     }
-    carrusel[0].style.display = "block";
-}
-
-function fboton2(){
-    for(let i=0; i<carrusel.length; i++){
-        carrusel[i].style.display = "none";
-    }
-    carrusel[1].style.display = "block";
-}
-
-function fboton3(){
-    for(let i=0; i<carrusel.length; i++){
-        carrusel[i].style.display = "none";
-    }
-    carrusel[2].style.display = "block";
+    carrusel[index].style.display = "block";
 }

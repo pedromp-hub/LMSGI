6119ac2c06a03bea61d8b87baa9951b3676a3eac
pedromp-hub <pmonpel2304@g.educaand.es>
2026-03-04 14:36:43 +0100

    ejercicio1 con temporaizador

--- JAVASCRIPT/Ejercicio1/ejercicio1.js
+++ JAVASCRIPT/Ejercicio1/ejercicio1.js
@@ -1,16 +1,35 @@
-let index = 0;
+/*MANUAL*/
+//let index = 0;
+//const carrusel = document.getElementsByClassName("imagen");
+//const boton1 = document.getElementById("boton1");
+//const boton2 = document.getElementById("boton2");
+//const boton3 = document.getElementById("boton3");
+
+//boton1.addEventListener('click', function(){fboton(0)});
+//boton2.addEventListener('click', function(){fboton(1)});
+//boton3.addEventListener('click', function(){fboton(2)});
+
+//function fboton(index){
+//    for(let i=0; i<carrusel.length; i++){
+//        carrusel[i].style.display = "none";
+//    }
+//    carrusel[index].style.display = "block";
+//}
+
+/*AUTOMATICO*/
+let diapositivaactual = 0;
 const carrusel = document.getElementsByClassName("imagen");
-const boton1 = document.getElementById("boton1");
-const boton2 = document.getElementById("boton2");
-const boton3 = document.getElementById("boton3");
+mostrarcarrusel();
 
-boton1.addEventListener('click', function(){fboton(0)});
-boton2.addEventListener('click', function(){fboton(1)});
-boton3.addEventListener('click', function(){fboton(2)});
-
-function fboton(index){
-    for(let i=0; i<carrusel.length; i++){
+function mostrarcarrusel(){
+    for (i = 0; i < carrusel.length; i++){
         carrusel[i].style.display = "none";
     }
-    carrusel[index].style.display = "block";
+    carrusel[diapositivaactual].style.display = "block";
+    diapositivaactual++;
+
+    if (diapositivaactual >= carrusel.length){
+        diapositivaactual = 0;
+    }
+    setTimeout(mostrarcarrusel, 2000);
 }
